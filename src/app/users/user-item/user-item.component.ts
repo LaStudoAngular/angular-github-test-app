36a86b26e7d5647e21cd16082db5b896088b0ad5
Valery Semenenko <gearmobile@gmail.com>
2018-11-08 02:06:37 +0400

    complete single user component

--- src/app/users/user-item/user-item.component.ts
+++ src/app/users/user-item/user-item.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'sc-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.scss']
 })
-export class UserItemComponent implements OnInit {
 
-  constructor() { }
+export class UserItemComponent implements OnInit {
+  user;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    // GET USERNAME FROM URL
+    this.route.params.subscribe(data => {
+      const username = data['user'];
+
+      // GET USER INFO FROM GITHUB
+      this.userService.getSingleUser(username)
+        .subscribe(user => this.user = user)
+    })
   }
 
 }
