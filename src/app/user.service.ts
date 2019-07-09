d4a44865ec8ab2d6c8a2016e91de9d295a6252af
Moe Green <gearmobile@gmail.com>
2019-07-09 22:47:32 +0300

    feature: use enviroment variable

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  apiURL = 'https://api.github.com/users';
-
   constructor(private http: HttpClient) {}
 
   // GET ALL USERS
   getAllUsers() {
-    return this.http.get(`${this.apiURL}?per_page=20`)
+    return this.http.get(`${environment.apiURL}?per_page=20`);
   }
 
   // GET SINGLE USER
-  getSingleUser ( username: string ) {
-    return this.http.get(`${this.apiURL}/${username}`)
+  getSingleUser(username: string) {
+    return this.http.get(`${environment.apiURL}/${username}`);
   }
 }
